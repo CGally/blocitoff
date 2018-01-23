ec2e532a202d784a8569e7d64990c92943e8a1ec
Chris Gallagher <cgally103@gmail.com>
2018-01-23 11:37:42 -0500

    create user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable, :validatable
+  :recoverable, :rememberable, :trackable, :confirmable, :validatable
+
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
+  resources :users, only: [:show]
+
   get 'about' => 'welcome#about'
+
+  authenticated :user do
+    root "users#show", as: :authenticated_root
+  end
 
   root 'welcome#index'
 
